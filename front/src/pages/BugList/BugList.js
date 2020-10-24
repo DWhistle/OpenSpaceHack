ead5d0c3141bc500c0a22d73de3513e5e77b1477
Koobi33 <koobi@mail.ru>
2020-10-24 12:37:00 +0300

    initial

---
+++ front/src/pages/BugList/BugList.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import styles from './BugList.module.less';
+
+const BugList = () => {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+};
+
+export default BugList;
