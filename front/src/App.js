0c7fe18524edcee752e779f45bf3c89af21d433c
Dasha Webb <DDmVebb@sberbank.ru>
2020-10-24 10:36:28 +0300

    init setup

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.less';
+
+function App() {
+  return (
+      <div className="App">
+          Here's your app
+      </div>
+  );
+}
+
+export default App;
