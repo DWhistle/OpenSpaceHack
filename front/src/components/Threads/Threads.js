ca4514f5ead6565f4eeb8cd182802bef571092f6
Dasha Webb <DDmVebb@sberbank.ru>
2020-10-24 22:17:16 +0300

    tags in tweets

--- front/src/components/Threads/Threads.js
+++ front/src/components/Threads/Threads.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import TabBar from '../TabBar'
 import SearchPanel from '../SearchPanel/SearchPanel'
 import styles from './Threads.module.less'
+import plusIcon from '../../icons/plus.svg'
 
 const items = [
     {
@@ -16,55 +17,113 @@ const items = [
 
 const threadsData = [
     {
-        tags: [],
-        date: '',
-        title: '',
-        description: '',
-        liked: [],
+        tags: [
+            {
+                name: 'переводы',
+                color: '#FF7F00',
+                id: 0,
+            },
+            {
+                name: 'iOs',
+                color: 'rgba(0, 212, 72, 0.4)',
+                id: 0,
+            },
+            {
+                name: 'авторизация',
+                color: 'rgba(219, 0, 44, 0.4)',
+                id: 0,
+            },
+        ],
+        date: '23.10.2020 в 19:34',
+        title: 'Опечатка в разделе "Дебетовые карты"',
+        description:
+            'Условное описание бага, может быть очень разным. Максимальное отображение в карточке треда – 1-2 строчки.',
+        liked: ['Жанна'],
         isActive: true,
     },
     {
-        tags: [],
-        date: '',
-        title: '',
-        description: '',
-        liked: [],
+        tags: ['Переводы', 'Опечатка', 'Срочно'],
+        date: '23.10.2020 в 19:34',
+        title: 'Опечатка в разделе "Дебетовые карты"',
+        description:
+            'Условное описание бага, может быть очень разным. Максимальное отображение в карточке треда – 1-2 строчки.',
+        liked: ['Жанна'],
+        isActive: true,
+    },
+    {
+        tags: ['Переводы', 'Опечатка', 'Срочно'],
+        date: '23.10.2020 в 19:34',
+        title: 'Опечатка в разделе "Дебетовые карты"',
+        description:
+            'Условное описание бага, может быть очень разным. Максимальное отображение в карточке треда – 1-2 строчки.',
+        liked: ['Жанна'],
+        isActive: true,
+    },
+    {
+        tags: ['Переводы', 'Опечатка', 'Срочно'],
+        date: '23.10.2020 в 19:34',
+        title: 'Опечатка в разделе "Дебетовые карты"',
+        description:
+            'Условное описание бага, может быть очень разным. Максимальное отображение в карточке треда – 1-2 строчки.',
+        liked: ['Жанна'],
         isActive: false,
     },
     {
-        tags: [],
-        date: '',
-        title: '',
-        description: '',
-        liked: [],
+        tags: ['Переводы', 'Опечатка', 'Срочно'],
+        date: '23.10.2020 в 19:34',
+        title: 'Опечатка в разделе "Дебетовые карты"',
+        description:
+            'Условное описание бага, может быть очень разным. Максимальное отображение в карточке треда – 1-2 строчки.',
+        liked: ['Жанна'],
         isActive: false,
     },
     {
-        tags: [],
-        date: '',
-        title: '',
-        description: '',
-        liked: [],
-        isActive: false,
-    },
-    {
-        tags: [],
-        date: '',
-        title: '',
-        description: '',
-        liked: [],
-        isActive: false,
+        tags: ['Переводы', 'Опечатка', 'Срочно'],
+        date: '23.10.2020 в 19:34',
+        title: 'Опечатка в разделе "Дебетовые карты"',
+        description:
+            'Условное описание бага, может быть очень разным. Максимальное отображение в карточке треда – 1-2 строчки.',
+        liked: ['Жанна'],
+        isActive: true,
     },
 ]
 
-const Tweet = () => {
-    return <div>я твит</div>
+const ExpandedTweet = () => {}
+
+const Tweet = ({ title, tags, description, liked, date }) => {
+    return (
+        <div className={styles.tweet}>
+            <div className={styles.top}>
+                <span className={styles.title}>{title}</span>
+                <div className={styles.tags}>
+                    {tags.map((tag) => (
+                        <div key={tag.id} className={styles.item} style={{ background: tag.color }}>
+                            <span>{tag.name}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className={styles.description}>{description}</div>
+            <div className={styles.bottom}>
+                <div className={styles.liked}>{liked}</div>
+                <span className={styles.date}>{date}</span>
+            </div>
+        </div>
+    )
 }
+
 const ThreadsContainer = ({ data }) => {
     return (
         <div className={styles.container}>
-            {data.map((item) => (
-                <Tweet />
+            {data.map((item, i) => (
+                <Tweet
+                    key={i}
+                    title={item.title}
+                    tags={item.tags}
+                    description={item.description}
+                    liked={item.liked}
+                    date={item.date}
+                />
             ))}
         </div>
     )
@@ -80,10 +139,6 @@ const Threads = () => {
         } else {
             setShowData(threadsData.filter((el) => !el.isActive))
         }
-    }, [activeItem])
-
-    useEffect(() => {
-        console.log(activeItem)
     }, [activeItem])
 
     return (
