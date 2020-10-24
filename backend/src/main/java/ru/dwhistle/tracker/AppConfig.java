2dd9b8996e47c941184376fa648554011b60bba8
a.shibaev <a.shibaev@ticketland.ru>
2020-10-25 02:39:10 +0300

    дотянул конфиг

--- backend/src/main/java/ru/dwhistle/tracker/AppConfig.java
+++ backend/src/main/java/ru/dwhistle/tracker/AppConfig.java
@@ -3,17 +3,20 @@ package ru.dwhistle.tracker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.context.annotation.RequestScope;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import ru.dwhistle.tracker.controller.util.UserContext;
 import ru.dwhistle.tracker.controller.util.UserAuthInterceptor;
+import ru.dwhistle.tracker.controller.util.UserContext;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.function.Predicate;
 
 @Configuration
 @EnableJpaRepositories("ru.dwhistle.tracker.backend.db")
@@ -25,7 +28,7 @@ public class AppConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .paths(Predicate.not((PathSelectors.regex("/error.*"))))
                 .build();
     }
 
@@ -36,9 +39,10 @@ public class AppConfig {
             public void addInterceptors(InterceptorRegistry registry) {
                 registry.addInterceptor(userAuthInterceptor)
                         .addPathPatterns("/**")
-                        .excludePathPatterns("auth/**",
-                        "/swagger-ui/**", "/configuration/**", "/swagger-resources/**",
-                        "/v2/api-docs", "/webjars/**");
+                        .excludePathPatterns("/auth/**",
+                                "/swagger-ui/**", "/configuration/**", "/swagger-resources/**",
+                                "/v2/api-docs", "/webjars/**")
+                        .pathMatcher(new AntPathMatcher());
             }
         };
     }
