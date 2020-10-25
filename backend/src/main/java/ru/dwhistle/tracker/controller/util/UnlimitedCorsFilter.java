196f3e6097f4d114ab2911cd9d4b49295f8b62a7
a.shibaev <a.shibaev@ticketland.ru>
2020-10-25 03:35:43 +0300

    CORS

---
+++ backend/src/main/java/ru/dwhistle/tracker/controller/util/UnlimitedCorsFilter.java
@@ -0,0 +1,21 @@
+package ru.dwhistle.tracker.controller.util;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class UnlimitedCorsFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(req, res);
+    }
+
+}
