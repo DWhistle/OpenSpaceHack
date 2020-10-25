6ecabbe695ea8b457f36bc8bbf9ffdd36760de4a
a.shibaev <a.shibaev@ticketland.ru>
2020-10-25 03:51:56 +0300

    CORS

--- backend/src/main/java/ru/dwhistle/tracker/AppConfig.java
+++ backend/src/main/java/ru/dwhistle/tracker/AppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -35,6 +36,10 @@ public class AppConfig {
     @Bean
     public WebMvcConfigurer webMvcConfigurer(UserAuthInterceptor userAuthInterceptor) {
         return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
             @Override
             public void addInterceptors(InterceptorRegistry registry) {
                 registry.addInterceptor(userAuthInterceptor)
