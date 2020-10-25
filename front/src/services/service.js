5debc2c591977989e97392dfb14f6abb49f6690d
Koobi33 <koobi@mail.ru>
2020-10-25 10:23:01 +0300

    asd

--- front/src/services/service.js
+++ front/src/services/service.js
@@ -2,16 +2,16 @@ import axios from 'axios';
 import handleErrors from './handleErrors';
 
 const service = {
-    get: (url, options) => axios.get(url, options).catch((error) => {
+    get: (url, options) => axios.get(url, { ...options, withCredentials: true }).catch((error) => {
         handleErrors(error);
     }),
-    post: (url, data, options) => axios.post(url, data, options).catch((error) => {
+    post: (url, data, options) => axios.post(url, data, { ...options, withCredentials: true }).catch((error) => {
         handleErrors(error);
     }),
-    put: (url, data, options) => axios.put(url, data, options).catch((error) => {
+    put: (url, data, options) => axios.put(url, data, { ...options, withCredentials: true }).catch((error) => {
         handleErrors(error);
     }),
-    delete: (url, options) => axios.delete(url, options).catch((error) => {
+    delete: (url, options) => axios.delete(url, { ...options, withCredentials: true }).catch((error) => {
         handleErrors(error);
     }),
 };
